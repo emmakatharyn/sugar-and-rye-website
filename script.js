4f678d7b1acbfcd6444d2180de74483a34d7c8e0
Emma Reed <ereed@easitpa.com>
2022-12-12 19:39:37 -0700

    original navbar stop point

---
+++ script.js
@@ -0,0 +1,7 @@
+// select mobile nav btn
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
