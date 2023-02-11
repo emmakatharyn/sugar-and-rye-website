2d845eea8272bbb2fae6f79ab2213ccbc2ac40a1
Emma Reed <ereed@easitpa.com>
2023-02-10 17:48:07 -0700

    make navbar stick once hero section is scrolled out of viewport

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
+// listen for click event on the hamburger icon
+// when clicked, we want the nav-open class to be applied to the header (if it's not already applied) or removed if it's already applied.
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// sticky nav
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
