6a27680d57ce8472866874576aeb4a176c3b5214
Emma Reed <ereed@easitpa.com>
2023-07-09 15:38:44 -0600

    fix mobile nav so that when a link is clicked, nav-open class is removed from the header

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // select mobile nav btn
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".main-nav-list a");
 
 // listen for click event on the hamburger icon
 // when clicked, we want the nav-open class to be applied to the header (if it's not already applied) or removed if it's already applied.
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    headerEl.classList.remove("nav-open");
+  });
 });
 
 // sticky nav
